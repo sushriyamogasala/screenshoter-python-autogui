e080df4110a515ddc33f8eca48270cd60e650ab1
Sushriya <sushriya1009@gmail.com>
2022-09-26 19:17:18 +0530

    Added Screenshots folder

--- Screenshotergui.py
+++ Screenshotergui.py
@@ -4,4 +4,4 @@ image=pyautogui.screenshot()
 
 image.show()
 
-image.save(r'C:\Users\sushr\OneDrive\Desktop\Projects\Screenshot using python\Using gui\ss5.png')
+image.save(r'C:\Users\sushr\OneDrive\Desktop\Projects\Screenshot using python\Using gui\screenshots\newss.png')
